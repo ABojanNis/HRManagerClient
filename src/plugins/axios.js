755604173a50e9e04a5581fc64f49bbd4d2aecf1
Boki <boki@larakicks.com>
2020-07-02 12:00:31 +0200

    Initial commit

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -4,6 +4,6 @@ import axios from "axios";
 // axios.defaults.baseURL = "http://localhost:3000/";
 
 // Heroku
-axios.defaults.baseURL = "http://agile-dawn-58615.herokuapp.com/";
+axios.defaults.baseURL = "https://agile-dawn-58615.herokuapp.com/";
 
 Vue.prototype.$http = axios;
