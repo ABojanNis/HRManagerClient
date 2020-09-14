4b4da7c5be82fcb56d713f975b9be64debb19d92
Boki <boki@larakicks.com>
2020-09-14 14:41:44 +0200

    second version

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,9 +1,9 @@
 import Vue from "vue";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/";
+// axios.defaults.baseURL = "http://localhost:3000/";
 
 // Heroku
-// axios.defaults.baseURL = "https://agile-dawn-58615.herokuapp.com/";
+axios.defaults.baseURL = "https://agile-dawn-58615.herokuapp.com/";
 
 Vue.prototype.$http = axios;
